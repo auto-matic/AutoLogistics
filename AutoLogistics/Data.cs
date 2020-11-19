6601c86943264fb4f941ee4629b365a4241a6513
Moritz Kessler <mrtz.ksslr@gmail.com>
2020-11-19 18:26:49 +0100

    Readded venv as it I see it as more convenient than having requirements for the users.

---
+++ AutoLogistics/Data.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoLogistics
+{
+    class Data
+    {
+    }
+}
--- WebcamQRCodeReader/main.py
+++ WebcamQRCodeReader/main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
-
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import cv2
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+def getqrcode():
+    cap = cv2.VideoCapture(0)
+    detector = cv2.QRCodeDetector()
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    while True:
+        _, img = cap.read()
+        data, bbox, _ = detector.detectAndDecode(img)
+        if bbox is not None:
+            if data:
+                print("QR Code detected, data:", data)
+                break
+    cap.release()
+    cv2.destroyAllWindows()
+    return data
