cd97f467ea6ac51df0841f1e762b28fa059337de
Moritz Kessler <mrtz.ksslr@gmail.com>
2020-11-25 18:31:30 +0100

    Added several basic structures and implemented the database structure.

---
+++ WebcamQRCodeReader/qr_code.py
@@ -0,0 +1,17 @@
+import cv2
+
+
+def get_qr_code():
+    cap = cv2.VideoCapture(0)
+    detector = cv2.QRCodeDetector()
+
+    while True:
+        _, img = cap.read()
+        data, bbox, _ = detector.detectAndDecode(img)
+        if bbox is not None:
+            if data:
+                print("QR Code detected, data:", data)
+                break
+    cap.release()
+    cv2.destroyAllWindows()
+    return data
